ef2bd82d35dbf4032f3ab5c046d36ba4823dc9c3
blnvk <blinovaaaaa19@gmail.com>
2023-09-29 23:39:36 +0300

    classes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+class point{
+public:
+    float x;
+    float y;
+
+    void print() {
+        std::cout << "x " << x << "\ty " << y << std::endl;
+    }
+};
+
 int main() {
-    std::cout << "Hello, world!" << endl;
+    point a;
+    a.x = 1;
+    a.y = 0.5;
+
+    a.print();
+
     return 0;
 }
