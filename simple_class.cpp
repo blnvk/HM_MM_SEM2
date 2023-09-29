94df937ea6f728c14d894fbcad78175a74743121
blnvk <blinovaaaaa19@gmail.com>
2023-09-30 00:02:12 +0300

    homework for first lesson

---
+++ simple_class.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class point {
+
+private:
+    float x;
+    float y;
+public:
+    void print() {
+        std::cout << "x " << x << "\ty " << y << std::endl;
+    }
+
+    float Get_x_data () {
+        return x;
+    }
+
+    void Set_x_value (float value_x) {
+        x = value_x;
+    }
+};
+
+int main() {
+    point a;
+    a.print();
+    return 0;
+}
